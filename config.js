19ebe8f2a1a4c4db915ac936f9f2ea6e931964ae
majxd9 <hamamajd43@gmail.com>
2026-07-28 00:31:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -69,5 +69,5 @@ const invitationData = {
   // ١) حطّي ملف الصوت بجانب index.html، مثلاً بمجلد اسمه audio
   // ٢) اكتبي مساره هون، مثلاً: "audio/song.mp3"
   // ٣) اتركيه فارغاً "" إذا ما بدك أي موسيقى — زر الصوت رح يختفي تلقائيًا
-  musicUrl: "hala/Alex Warren - Ordinary (Karaoke Version)(MP3_160K).mp3"
+  musicUrl: "main/hala/Alex Warren - Ordinary (Karaoke Version)(MP3_160K).mp3"
 };
